9686a04015f6eab7e52b4c1cedc21b2896c85284
audrey murua <audrey.murua1@gmail.com>
2021-12-10 14:39:48 -0600

    new branch, update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const API_URL = 'https://cedar-quirky-rest.glitch.me/movies';
+const API_URL = 'https://bird-showy-spur.glitch.me/movies';
 
 //Get All Movies
 let getALLMovies = () => fetch(API_URL).then(resp => resp.json()).catch(err => console.error(err));
@@ -25,7 +25,7 @@ let editMovies = (movie) => {
 		// A request body - Additional content that is included along with our request, usually a JSON string that represents the resource(s) being created/modified.
 		body: JSON.stringify(movie) // Convert the JS object into a JSON string before sending it to the server.
 	}
-	
+
 	return fetch(`${API_URL}/${movie.id}`, options).then(resp => resp.json()).catch(err => console.error(err));
 }
 
@@ -45,23 +45,36 @@ let editedMovie = {
 
 //POST REQUEST
 
-let addMovie = {
+let newMovie = {
 	title: 'Avatar',
-	rating: 10,
+	rating: '10',
 	id: 0,
 }
 
-
 let createdMovie = movie => {
-	//options inside function as a built-in vs selecting one from the outside
 	let options = {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify(movie)
+	};
+	return fetch(API_URL, options).then(resp => resp.json()).catch(err => console.error(err));
+}
+// createdMovie(newMovie).then(data => console.log(data));
+
+
+// DELETE REQUEST
+
+let deleteMovie = (id) => {
+	let options = {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json'
+		}
 	}
-	return fetch(API_URL, options).then(response => response.json()).catch(err => console.error(err));
+	return fetch(`${API_URL}/${id}`, options).then(resp => resp.json()).catch(err => console.error(err))
 }
 
-createdMovie(addMovie).then(data => console.log(data)).catch(err => console.error(err));
+
+deleteMovie(16);
