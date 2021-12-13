89c8b43c5f67df2822e7784e4940a9b79ca4d07d
Charles Aggasid <charles.aggasid01@gmail.com>
2021-12-13 12:02:49 -0600

    addeing filter

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ let createdMovie = movie => {
 /** EDIT A POST - PUT  is for checking if resource exists then update , else create new resource **/
 
 let editedMovie = {
-	"title": "Guardians of the Galaxy Vol. 2",
+	"title": "Movie",
+	"id:": 6
 }
 let editMovies = (movie) => {
 	let options = {
@@ -63,8 +64,8 @@ let editMovies = (movie) => {
 
 /***************************** PATCH is always for update a resource ***********************************/
 let patchMovie = {
-		"title": "Guardians of the Galaxy Vol. 2 ",
-		"id": 5
+		"title": "Black Hawk Down",
+		"id": 2
 	}
 let patchMovies = (movie) => {
 	let options = {
@@ -113,16 +114,212 @@ When the initial AJAX request comes back, remove the "loading..." message and re
 
 // START APPENDING HTML =================================
 
-// const loadingPromise = fetch(API_URL);
-// const main = document.querySelector('.card');
-// main.innerHTML = "<p>Loading....";
-//
-// loadingPromise.then(res => res.json()).then(movies => main.innerHTML = getALLMovies(movies));
 
-// getALLMovies().then(data => console.log(data));
-// }
+	// url for Glitch Movie API
+	const url = "https://bird-showy-spur.glitch.me/movies"
+	const mainRow = $('.main-row')
+	const loading = $('.loading')
+	const addBtn = $('#addMovie')
+	let movieModal = $('.modal-div')
+	let movieHTML = ""
+	let modalHTML = ""
+	const deleteOptions = {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}
+	
+	const fetchMovie = (delay) => {
+		$('.loading-container').css('margin-top', '150px')
+		loading.toggle('hidden')
+		mainRow.toggle('hidden')
+		setTimeout(function(){
+			fetch(url)
+				.then(res => res.json())
+				.then(data => {
+					renderHTML(data);
+				})
+				.then($('.loading-container').css('margin-top', '0'))
+				.then(loading.toggle('hidden'))
+				.then(mainRow.toggle('hidden'))
+				.catch(error => console.error(error))
+		}, delay)
+	}
 
+	const renderHTML = data => {
+	movieHTML = ""
+	modalHTML = ""
+	createModal(data)
+	for(let ele of data) {
+		movieHTML += `<div class="col-12 col-md-6 col-lg-4 movie-columns">
+                            <div class="card" style="width: 18rem;">
+                                <img id="movie${ele.id}" src="${ele.poster}" class="card-img-top" alt="Movie Poster" style="height: 100%; width: auto">
+                                <div class="info${ele.id} hidden">
+                                <div class="card-body">
+                                <h5 class="card-title">${ele.title}</h5>
+                                <p class="card-text">${ele.plot}</p>
+                                <p class="card-text">Rating: ${ele.rating}</p>
+                                <p class="card-text">Release Year: ${ele.year}</p>
+                                <p class="card-text">Genres: ${ele.genre}</p>
+                                <button class="btn-block btn btn-warning edit-data${ele.id}" data-toggle="modal" data-target="#editModal${ele.id}">Edit</button>
+                                <button id="deleteMovie${ele.id}" class="btn-block btn btn-warning">Delete</button>
+                            </div></div></div></div>`
+	}
+	mainRow.html(movieHTML)
+	for(let ele of data) {
+		$(`#deleteMovie${ele.id}`).click(function () {
+			$(`#deleteMovie${ele.id}`).attr('disabled')
+			let userDelete = confirm(`Are you sure you want to delete ${ele.title}?`)
+			// DELETE FETCH
+			if(userDelete){
+				fetch(`${url}/${ele.id}`, deleteOptions)
+					.then(res => res.json())
+					.then(data => console.log(data))
+					.then(fetchMovie(2000))
+					.then($(`#deleteMovie${ele.id}`).removeAttr('disabled'))
+					.catch(error => console.error(error))
+			} else {
+				$(`#deleteMovie${ele.id}`).removeAttr('disabled')
+			}
+		})
+		$(`#editMovie${ele.id}`).click(function () {
+			$(`#editMovie${ele.id}`).attr('disabled');
+			let userTitle = $(`#titleInput${ele.id}`).val()
+			let userRating = $(`#ratingSelect${ele.id}`).val()
+			let userYear = $(`#yearInput${ele.id}`).val()
+			let userGenre = $(`#genreInput${ele.id}`).val()
+			let userPlot = $(`#plotInput${ele.id}`).val()
+			let userPoster =$(`#posterInput${ele.id}`).val()
+			let userMovie = {
+				title: userTitle,
+				rating: userRating,
+				year: userYear,
+				genre: userGenre,
+				plot: userPlot,
+				poster: userPoster
+			}
+			const patchOptions = {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(userMovie)
+			}
+			fetch(`${url}/${ele.id}`, patchOptions)
+				.then(res => res.json())
+				.then(data => console.log(data))
+				.then(fetchMovie(2000))
+				.then($(`#editMovie${ele.id}`).removeAttr('disabled'))
+				.catch(error => console.error(error))
+		})
+		$(`#movie${ele.id}`).click(function(){
+			$(`.info${ele.id}`).toggle('hidden')
+			$(`#movie${ele.id}`).toggle('hidden')
+		})
+		$(`.info${ele.id}`).click(function(){
+			$(`.info${ele.id}`).toggle('hidden')
+			$(`#movie${ele.id}`).toggle('hidden')
+		})
+	}
+}
 
-//poster, title, plot, year, rating, actors, director
+	const createModal = data => {
+	for(let ele of data) {
+		modalHTML += `<div class="modal fade" id="editModal${ele.id}" tabindex="-1" role="dialog" aria-labelledby="${ele.id}ModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="${ele.id}ModalLabel">Edit ${ele.title}</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                     <input id="titleInput${ele.id}" type="text" class="form-control mb-2 mr-sm-2" value="${ele.title}">
+                                        <input id="yearInput${ele.id}" type="text" class="form-control mb-2 mr-sm-2" value="${ele.year}">
+                                        <input id="genreInput${ele.id}" type="text" class="form-control mb-2 mr-sm-2" value="${ele.genre}">
+                                        <input id="posterInput${ele.id}" type="text" class="form-control mb-2 mr-sm-2" value="${ele.poster}">
+                                        
+                                        <div class="input-group mb-2 mr-sm-2">
+                                            <div class="input-group-prepend">
+                                                <div class="input-group-text">Rating</div>
+                                            </div>
+                                            <select id="ratingSelect${ele.id}" class="form-control">`
+		if (ele.rating === '1') {
+			modalHTML += `<option selected>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>`
+		} else if (ele.rating === '2') {
+			modalHTML += `<option>1</option>
+                         <option selected>2</option>
+                         <option>3</option>
+                         <option>4</option>
+                         <option>5</option>`
+		} else if (ele.rating === '3') {
+			modalHTML += `<option>1</option>
+                          <option>2</option>
+                          <option selected>3</option>
+                          <option>4</option>
+                          <option>5</option>`
+		} else if (ele.rating === '4') {
+			modalHTML += `<option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option selected>4</option>
+                          <option>5</option>`
+		} else if (ele.rating === '5') {
+			modalHTML += `<option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option selected>5</option>`
+		}
+		modalHTML += `</select>
+                            </div>
+                           <textarea id="plotInput${ele.id}" type="text" class="form-control mb-2 mr-sm-2">${ele.plot}</textarea>
+                  <div class="modal-footer">
+                       <button id="editMovie${ele.id}" class="btn btn-primary" data-dismiss="modal">Submit</button>
+                       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  </div></div></div></div></div>`
+	}
+	movieModal.html(modalHTML)
+}
+
+//ADD BUTTON COMPLETE
+	addBtn.click(() => {
+	addBtn.attr('disabled');
+	let userTitle = $('#titleInput').val()
+	let userRating = $('#ratingSelect').val()
+	let userYear = $('#yearInput').val()
+	let userGenre = $('#genreInput').val()
+	let userPlot = $('#plotInput').val()
+	let userPoster = $('#posterInput').val()
+	let userMovie = {
+		title: userTitle,
+		rating: userRating,
+		year: userYear,
+		genre: userGenre,
+		plot: userPlot,
+		poster: userPoster
+	}
+	const postOptions = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(userMovie)
+	}
+	fetch(url, postOptions)
+		.then(res => res.json())
+		.then(data => console.log(data))
+		.then(fetchMovie(2000))
+		.then(addBtn.removeAttr('disabled'))
+		.catch(error => console.error(error))
+})
+
+	fetchMovie(6000);
 
 
